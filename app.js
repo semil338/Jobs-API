b1d97a142cb3a7eafec3d9b808983d1e7c063f38
Semil Khedawala <semilkhedawala@gmail>
2023-07-17 20:24:27 +0530

    security packages added

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 require("dotenv").config();
 require("express-async-errors");
+
+// extra security packages
+
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -16,7 +24,18 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
+
 // extra packages
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
